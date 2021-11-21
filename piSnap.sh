ab85272c1560fd690c15046aa4aef3252919de02
Mitch Richling <http://www.mitchr.me/>
2021-11-21 16:17:08 -0600

    New functionality

--- piSnap.sh
+++ piSnap.sh
@@ -35,45 +35,44 @@ read -r -d '' HELPT <<EOF
 Take one (or more) snapshot(s) using the Raspberry Pi HQ Camera and save them off in a standard way
 
 Use: piSnap.sh [options] [file-annotation]
-       Options: -k      Show a preview, and capture on keypress.
-                        An image is captured for [enter], exit with [x] followed by [enter]
-                -f      Printf format for numeric index in filenames created with -k option
-                        Default: %02d
-                -p      Preview only.  No images are captured. All other arguments are ignored
-                -s      Show image(s) after capture with nomacs (my favorite lightweight image viewer)
+       Options: -p      Preview only.  No images are captured. All other arguments are ignored
+                -k      Show a preview, and capture when [enter] is pressed.  
+                        Without the -k option an image is immediatly captured with no preview
+                -s      Show image after capture with nomacs (my favorite lightweight image viewer)
                 -v      Verbose mode
                 -b BIN  Full path to the libcamera-still binary
                         Default: /usr/bin/libcamera-still
                 -d DIR  Directory to store captured images.  
                         Default: $HOME/Pictures/pi-cam
                         Note: The related ImageJ/Fiji macro expects the default value!
-                -e ENC  File format: jpg, bmp, gif, png
+                -e ENC  File format: jpg, bmp, gif, png, rgb
                         Default: jpg
-
-Image names are like: YYYYMMDDHHMMSS_COUNT-ANNOTATION.ENC -- Note the _COUNT and/or -ANNOTATION bits may be missing.
-
-Note: Older versions of the image capture tool would capture multiple images with -k option.  The current versions don't 
-      work like this.  I'm waiting to see if this functionality comes back.  If it looks like single capture is the 
-      future, then I'll rework this code a bit.
+       Annotation
+           The file-annotation argument is translated automatically into something suitable for a 
+           file name.  White space and non-printable characters are converted to underscores.  
+           Multiple underscores are reduced to a single underscore.
+       File Names
+           Image names are like: YYYYMMDDHHMMSS-ANNOTATION.ENC
 
 EOF
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
+FAKE_CAP='Y'
 SHOW='N'
-PFMT='%02d'
-MULTI='N'
+WKEY='N'
 VERB='N'
 PREVIEW='N'
 ANNOT=''
+OANNOT=''
 ODIR="$HOME/Pictures/pi-cam"
-IFMT='jpg'
+IENC='jpg'
 RASPISP='/usr/bin/libcamera-still'
 while [[ "$1" = -* ]]; do
    case "$1" in
-    -k ) MULTI='Y';                                             ;; # Capture multiple images
+    -k ) WKEY='Y';                                             ;; # Capture multiple images
     -d ) ODIR="$2"; shift;                                      ;; # Output directory
     -v ) VERB='Y';                                              ;; # Verbose mode
-    -e ) IFMT="$2"; shift;                                      ;; # Output image format
+    -e ) IENC="$2"; shift;                                      ;; # Output image format
     -s ) SHOW='Y';                                              ;; # Open captured images
     -p ) PREVIEW='Y';                                           ;; # Preview only
     -b ) RASPISP="$2"; shift;                                   ;; # Location of libcamera-still binary
@@ -82,17 +81,19 @@ while [[ "$1" = -* ]]; do
    shift;
 done
 if [ -n "$1" ]; then
-  ANNOT="-$1"
+  OANNOT="$1"
+  ANNOT="-"`echo -n "$OANNOT" | tr -sc '[[:graph:]]' '_'`
 fi
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 if [ "$VERB" = 'Y' ]; then                     
-  echo "DEBUG: MULTI    $MULTI    "
+  echo "DEBUG: WKEY     $WKEY     "
   echo "DEBUG: VERB     $VERB     " 
   echo "DEBUG: PREVIEW  $PREVIEW  "
   echo "DEBUG: ANNOT    $ANNOT    "
+  echo "DEBUG: OANNOT   $OANNOT   "
   echo "DEBUG: ODIR     $ODIR     " 
-  echo "DEBUG: IFMT     $IFMT     "
+  echo "DEBUG: IENC     $IENC     "
 fi    
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -102,16 +103,22 @@ if [ -n "$2" ]; then
   exit
 fi
 
-if [[ ! "$IFMT" =~ ^(jpg|bmp|gif|png)$ ]]; then
-  echo "ERROR: Encodeing of '$IFMT' is not supported!"
+if [[ ! "$IENC" =~ ^(jpg|bmp|gif|png|rgb)$ ]]; then
+  echo "ERROR: Encodeing of '$IENC' is not supported!"
   echo "$HELPT"
   exit
 fi
 
 if [ ! -x "$RASPISP" ]; then
-  echo "ERROR: $RASPISP not found!"
-  echo "$HELPT"
-  exit
+  if [ "$FAKE_CAP" = 'Y' ]; then
+    if [ "$VERB" = 'Y' ]; then                     
+      echo "DEBUG: In FAKE_CAP mode. Didn't find $RASPISP"
+    fi
+  else
+    echo "ERROR: $RASPISP not found!"
+    echo "$HELPT"
+    exit
+  fi
 fi
 
 if [ ! -d "$ODIR" ]; then
@@ -126,35 +133,53 @@ if [ ! -d "$ODIR" ]; then
 fi
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
+DACMD="$RASPISP"
+OFILE=''
+if [ "$IENC" = "raw" ]; then
+  DACMD="$DACMD -r"
+  IENC='jpg'
+fi
+
 if [ "$PREVIEW" = 'Y' ]; then
-  DACMD="$RASPISP -t 0"
+  DACMD="$DACMD -t 0"
 else
-  if [ "$MULTI" = "Y" ]; then
-    FILE=$ODIR'/'`date '+%Y%m%d%H%M%S'`'_'${PFMT}${ANNOT}'.'$IFMT
-    MARG='-t 0 -k'
+  if [ "$WKEY" = "Y" ]; then
+    DACMD="$DACMD -t 0 -k"
   else
-    FILE=$ODIR'/'`date '+%Y%m%d%H%M%S'`${ANNOT}'.'$IFMT
-    MARG='-t 1 -n'
+    DACMD="$DACMD -t 1 -n"
   fi
-  DACMD="$RASPISP $MARG -q 100 -e $IFMT -o $FILE"
+  if [ "$IENC" = "jpg" ]; then
+    DACMD="$DACMD -q 100"
+  fi
+  OFILE=$ODIR'/'`date '+%Y%m%d%H%M%S'`${ANNOT}'.'$IENC
+  DACMD="$DACMD -e $IENC -o $OFILE"
 fi
 if [ "$VERB" = 'Y' ]; then                     
   echo "DEBUG: Command to run: $DACMD"
 fi
+if [ "$FAKE_CAP" = 'Y' ]; then
+  if [ "$PREVIEW" = 'Y' ]; then
+    DACMD='true'
+  else
+    DACMD="convert -size 1024x1024 xc:white $OFILE"
+  fi
+  if [ "$VERB" = 'Y' ]; then                     
+    echo "DEBUG: In FAKE_CAP mode. New command to run: $DACMD"
+  fi
+fi
 $DACMD
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-TFILE=`printf "$FILE" 0`
-if [ -e "$TFILE" ]; then
-  echo "INFO: Captured Image File(s):"
-  ls -l `echo "$TFILE" | sed 's/_00*/_*/'`
+if [ -n "$OFILE" -a -e "$OFILE" ]; then
+  echo "INFO: Captured Image File:"
+  ls -l "$OFILE" | sed 's/^/    /'
   if [ "$SHOW" = 'Y' ]; then
     if [ -x '/usr/bin/nomacs' ]; then
-      /usr/bin/nomacs `echo "$TFILE" | sed 's/_0/_*/'`
+      /usr/bin/nomacs "$OFILE"
     else
-      echo 'ERROR: Unable to open images (/usr/bin/nomacs) not found.'
+      echo 'ERROR: Unable to open image (/usr/bin/nomacs) not found.'
     fi
   fi
 else
-  echo "ERROR: No image(s) captured!"
+  echo "ERROR: No image captured!"
 fi
